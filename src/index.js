268bf682e9ca378fd044e82f57a5dd24c5d92e6c
natalie916 <natalietendai916@gmail.com>
2025-02-24 13:24:34 +0200

    search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let weatherElement = document.querySelector("#weather");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  weatherElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "67f4ad1a7091d3bd59036ato3f25bee1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Cape Town");
